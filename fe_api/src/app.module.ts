13b14ebb8e9c09b05b56a4dded3688832401e4d8
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-26 00:18:50 +0100

    permisos trabajo con token

--- fe_api/src/app.module.ts
+++ fe_api/src/app.module.ts
@@ -4,7 +4,9 @@ import { AppService } from './app.service';
 import { JobModule } from './job/job.module';
 import { ConfigModule } from '@nestjs/config';
 import { StatusJobModule } from './status-job/status-job.module';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -13,6 +15,11 @@ import configuration from './config/configuration';
       load: [configuration],
     }),
     StatusJobModule,
+    AuthModule,
+    JwtModule.register({
+      secret: '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', // Reemplaza con tu clave secreta
+      signOptions: { expiresIn: '1h' }, // Puedes ajustar la expiración según tus necesidades
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- fe_api/src/job/job.controller.ts
+++ fe_api/src/job/job.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Param, Post } from '@nestjs/common';
 import { JobService } from './job.service';
 import { sendWorkDto } from './dto/sendWork';
 import { ApiTags } from '@nestjs/swagger';
@@ -9,7 +9,13 @@ import { responseDto } from './dto/response';
 export class JobController {
   constructor(private readonly appService: JobService) {}
   @Post('sendWork')
-  async sendWork(@Body() sendWork: sendWorkDto): Promise<responseDto> {
+  async sendWork(
+    @Headers() headers,
+    @Body() sendWork: sendWorkDto,
+  ): Promise<responseDto> {
+    const forwardedEmail = headers['x-forwarded-email'];
+    console.log('cabecera', headers);
+    console.log('email', forwardedEmail);
     return this.appService.sendWork(sendWork);
   }
   @Get(':id')
