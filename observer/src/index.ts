5d03efe3f794976976f1c3ddd4ad6491de724896
David Valdaliso Lopez <dvaldaliso@gmail.com>
2024-01-26 13:50:28 +0100

    varaible de entorno para la cantidad de procesos en cola permitodo

--- observer/src/index.ts
+++ observer/src/index.ts
@@ -11,14 +11,14 @@ interface WorkerData {
 const workerData: { [key: string]: WorkerData } = {};
 
 async function main(): Promise<void> {
+  const NUM_PROCESOS_IN_QUEUE_FOR_OTHER_WORkER=process.env.NUM_PROCESOS_IN_QUEUE_FOR_OTHER_WORkER || '5'
   console.log("inicializado"+process.env.NATS_URL)
   setInterval(async () => {
-    let resp;
     try {
       const resp = await axios.get(`http://${process.env.NATS_URL}:${process.env.NATS_PORT}/varz`);
       console.log("here");
       console.log(resp.data.in_msgs);
-      if (resp.data.in_msgs > 10) {
+      if (resp.data.in_msgs > parseInt(NUM_PROCESOS_IN_QUEUE_FOR_OTHER_WORkER)) {
         console.log('Adicionar nuevo worker');
       //workerData[crypto.randomUUID()] = { start: undefined, end: new Date() };
       }else{
